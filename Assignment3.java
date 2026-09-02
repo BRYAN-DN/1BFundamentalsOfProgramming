4830f9eaad6219986e93260e0a9c47d031218465
Bryan Dingle <bd365868@gmail.com>
2026-09-02 23:20:41 +0800

    My Info

---
+++ Assignment3.java
@@ -0,0 +1,21 @@
+public class Assignment3 { // Declared a public class named Assignment3
+
+    public static void main (String[] args){ // This is where the program begins
+
+    String firstname= "Bryan"; // Declared a variable that will store my firstname
+    String lastname= "Dingle"; // Declared a vaariable that will store my lastname
+    String address= "Zone 5 Lipay Villasis Pangasinan"; // Declared a variable that will store my address
+    String mobilenumb= "09774658802"; // Declared a variable that will store my phone number
+    int age= 18; // Declared a int varriable that will store my age
+    double gwa= 93.5; // Declared a double variable that will store my GWA
+
+        System.out.println("Firstname: "+firstname); // declared a syntax that will print my name
+        System.out.println("Lastname: "+lastname); // declared a syntax that will print my lastname
+        System.out.println("Address: "+address); // declared a syntax that will print my address
+        System.out.println("Mobile Number: "+mobilenumb); // declared a syntax that will print my mobile number
+        System.out.println("Age: "+age); // declared a syntax that will print my age
+        System.out.println("GWA: "+gwa); // declared a syntax that will print my GWA
+    } // this is where the execution will end
+
+
+} // this is where the program ends
